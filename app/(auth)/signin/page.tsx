447d148b58dd3626bf1cb75e2209aaa28037b376
darshansrc <darshansr1618@gmail.com>
2024-12-01 14:19:52 +0530

    fix: login page ui

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,13 +1,16 @@
+
 import LoginForm from "@/components/login-form";
 import { ModeToggle } from "@/components/theme-toggle";
+import { ShootingStars } from "@/components/shooting-stars";
+import { StarsBackground } from "@/components/stars-background";
 
 export default function Page() {
+
   return (
-    <div className="flex relative h-screen w-full items-center bg-muted/40 justify-center px-4">
-      <div className="absolute top-4 right-4">
-        <ModeToggle className="border-none bg-transparent shadow-none" />
-      </div>
-      <LoginForm />
+    <div className="h-screen  rounded-md bg-muted/40 flex flex-col items-center justify-center relative w-full">
+     <LoginForm />
+      <ShootingStars />
+      <StarsBackground />
     </div>
   );
 }
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -11,45 +11,70 @@ import {
 } from "@/components/ui/card";
 
 import { GithubIcon, GoogleIcon } from "./icons";
-import { BrainCircuit, Brain } from "lucide-react";
+import { BrainCircuit, Loader } from "lucide-react";
+import { useState } from "react";
 
 export default function LoginForm() {
+  const [isGoogleLoading, setIsGoogleLoading] = useState(false);
+  const [isGithubLoading, setIsGithubLoading] = useState(false);
   const handleGoogleSignIn = () => {
-    signIn("google", { callbackUrl: "/" });
+    setIsGoogleLoading(true);
+
+    signIn("google", { callbackUrl: "/" })
   };
 
   const handleGithubSignIn = () => {
-    signIn("github", { callbackUrl: "/" });
+    setIsGithubLoading(true);
+
+    signIn("github", { callbackUrl: "/" })
+
   };
 
   return (
-    <Card className="w-[350px]">
+    <Card className="w-[350px] bg-transparent border-none shadow-none">
       <CardHeader>
-        <CardTitle className="flex flex-row items-center gap-2 pb-6">
+        <div>
+        <CardTitle className="flex pb-2 flex-row items-center gap-2 ">
           <BrainCircuit />
           ResearchLM
         </CardTitle>
-        <CardTitle>Login / Signup</CardTitle>
         <CardDescription>
-          Continue with your preferred login method.
+        Your AI-powered research assistant for analyzing data, taking notes, and creating diagrams from various sources including CSV, PDF, and more.
         </CardDescription>
+
+        </div>
+
       </CardHeader>
       <CardContent>
         <div className=" space-y-2">
           <Button
             variant="outline"
+            disabled={isGoogleLoading}
             className="w-full bg-muted/40"
             onClick={handleGoogleSignIn}
           >
-            <GoogleIcon className="mr-2 h-4 w-4" />
+            {
+              isGoogleLoading ? (
+                  <Loader className="mr-1 h-4 w-4 animate-spin" />
+              ) : (
+                <GoogleIcon className="mr-1 h-4 w-4" />
+              )
+            }
             Continue with Google
           </Button>
           <Button
             variant="outline"
+            disabled={isGithubLoading}
             className="w-full  bg-muted/40"
             onClick={handleGithubSignIn}
           >
-            <GithubIcon className="mr-2 h-4 w-4" />
+            {
+              isGithubLoading ? (
+                  <Loader className="mr-1 h-4 w-4 animate-spin" />
+              ) : (
+                <GithubIcon className="mr-1 h-4 w-4" />
+              )
+            }
             Continue with GitHub
           </Button>
         </div>
